586e0d0a3a5f93788424cca942654d281905f3f7
Danilo Ambrosio <danilo.ambrosio@focusnetworks.com.br>
2017-07-12 00:18:02 -0300

    Mocking schedule btn

--- user-interface/public/assets/js/app/resource-schedule.js
+++ user-interface/public/assets/js/app/resource-schedule.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
+
   $('.datepicker').datepicker({
     autoclose: true
   });
+
+  $('#schedule-btn').click(function(){
+    $('#resource-schedule-modal').modal('hide');
+    toastr.info('Recurso agendado com sucesso!','');
+  });
+
 });
